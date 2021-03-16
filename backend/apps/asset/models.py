3c58341a90e42150868c169dc30133bb05c4387a
RichardDuranoMarrony <r.marrony@hoptmail.f>
2021-03-16 17:27:56 +0100

    initialize supplier and documentation

--- backend/apps/asset/models.py
+++ backend/apps/asset/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from ..team.models import Team
+from ..supplier.models import Supplier
 # Create your models here.
+
+
 class Asset(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=200)
-    picture = models.ImageField(null= True,blank = True)
+    picture = models.ImageField(null=True, blank=True)
     cost = models.DecimalField(max_digits=16, decimal_places=2)
-    # supplier = models.ForeignKey()
-    team = models.ForeignKey(Team, on_delete= models.SET_NULL, null = True, blank= True)
+    supplier = models.ForeignKey(
+        Supplier, on_delete=models.SET_NULL, null=True)
+    team = models.ForeignKey(
+        Team, on_delete=models.SET_NULL, null=True, blank=True)
+
     def __str__(self):
         return self.name
--- backend/project/schema.py
+++ backend/project/schema.py
@@ -1,9 +1,10 @@
 import graphene
 
 from apps.organization import schema as organization_schema
+from apps.supplier import schema as supplier_schema
 
 
-class Query(organization_schema.Query, graphene.ObjectType):
+class Query(supplier_schema.Query, organization_schema.Query, graphene.ObjectType):
     pass
 
 
