cd07fb15a15e71b8cdddb7707c8a1b536648bb18
Sami <sami.belkeir@gmail.com>
2021-03-16 17:23:23 +0100

    add schema for assets and teams

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -1,10 +1,17 @@
 import graphene
 
 from apps.organization import schema as organization_schema
+from apps.team import schema as team_schema
+from apps.asset import schema as asset_schema
 
 
 class Query(organization_schema.Query, graphene.ObjectType):
     pass
 
+class Query(team_schema.Query, graphene.ObjectType):
+    pass
+
+class Query(asset_schema.Query, graphene.ObjectType):
+    pass
 
 schema = graphene.Schema(query=Query)
