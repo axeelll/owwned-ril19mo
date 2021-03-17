63e0217bec0e5683f6162fc3593c4e10d51d085b
Pablo <pabloarrieta@outlook.fr>
2021-03-17 09:21:24 +0100

    changed schema in histoy-event

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -4,9 +4,10 @@ from apps.organization import schema as organization_schema
 from apps.supplier import schema as supplier_schema
 from apps.team import schema as team_schema
 from apps.asset import schema as asset_schema
+from apps.history_event import schema as history_event_schema
 
 
-class Query(asset_schema.Query, team_schema.Query, supplier_schema.Query, organization_schema.Query, graphene.ObjectType):
+class Query(history_event_schema.Query, asset_schema.Query, team_schema.Query, supplier_schema.Query, organization_schema.Query, graphene.ObjectType):
     pass
 
 
