79f556be1fdd3a09eb24e357928262440b77cfc7
AnthonyDeniau <anthony.deniau@gmail.com>
2021-03-17 10:30:42 +0100

    add CUD for organization

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -7,8 +7,14 @@ from apps.asset import schema as asset_schema
 from apps.history_event import schema as history_event_schema
 
 
-class Query(history_event_schema.Query, asset_schema.Query, team_schema.Query, supplier_schema.Query, organization_schema.Query, graphene.ObjectType):
+class Query(history_event_schema.Query, asset_schema.Query, team_schema.Query,
+            supplier_schema.Query, organization_schema.Query,
+            graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(organization_schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
